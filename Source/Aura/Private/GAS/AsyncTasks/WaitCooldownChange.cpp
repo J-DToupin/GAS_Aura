f96584c7fab5439e08ab10be104ebdb9c595cca3
JDToupin <JD2142@gmail.com>
2023-12-06 16:21:08 -0500

    Wait Cooldown UI

---
+++ Source/Aura/Private/GAS/AsyncTasks/WaitCooldownChange.cpp
@@ -0,0 +1,65 @@
+﻿// Copyright JDgame
+
+
+#include "GAS/AsyncTasks/WaitCooldownChange.h"
+
+#include "AbilitySystemComponent.h"
+
+UWaitCooldownChange* UWaitCooldownChange::WaitForCooldownChange(UAbilitySystemComponent* AbilitySystemComponent,
+                                                                const FGameplayTag& InCooldownTag)
+{
+	UWaitCooldownChange* WaitCooldownChange = NewObject<UWaitCooldownChange>();
+	WaitCooldownChange->AbilitySystemC = AbilitySystemComponent;
+	WaitCooldownChange->CooldownTag = InCooldownTag;
+
+	if (!IsValid(AbilitySystemComponent) || !InCooldownTag.IsValid())
+	{
+		WaitCooldownChange->EndTask();
+		return nullptr;
+	}
+
+	// Pour savoir quand un cooldown est fini
+	AbilitySystemComponent->RegisterGameplayTagEvent(InCooldownTag).AddUObject(WaitCooldownChange,
+		&UWaitCooldownChange::CooldownTagChanged);
+
+	// Pour savoir quand un cooldown effect est appliquer
+	AbilitySystemComponent->OnActiveGameplayEffectAddedDelegateToSelf.AddUObject(WaitCooldownChange, &UWaitCooldownChange::OnActiveEffectAdded);
+
+	return WaitCooldownChange;
+}
+
+void UWaitCooldownChange::EndTask()
+{
+	if (!IsValid(AbilitySystemC)) return;
+	
+	AbilitySystemC->RegisterGameplayTagEvent(CooldownTag).RemoveAll(this);
+
+	SetReadyToDestroy();
+	MarkAsGarbage();
+}
+
+void UWaitCooldownChange::CooldownTagChanged(const FGameplayTag InCooldownTag, int32 NewCount)
+{
+	if (NewCount == 0)
+	{
+		CooldownEndDelegate.Broadcast(0.f);
+	}
+}
+
+void UWaitCooldownChange::OnActiveEffectAdded(UAbilitySystemComponent* TargetASC,
+	const FGameplayEffectSpec& SpecApplied, FActiveGameplayEffectHandle EffectHandle)
+{
+	FGameplayTagContainer AssetTags;
+	SpecApplied.GetAllAssetTags(AssetTags);
+
+	FGameplayTagContainer GrantedTags;
+	SpecApplied.GetAllGrantedTags(GrantedTags);
+
+	if (AssetTags.HasTagExact(CooldownTag) || GrantedTags.HasTagExact(CooldownTag))
+	{
+		FGameplayEffectQuery GameplayEffectQuery = FGameplayEffectQuery::MakeQuery_MatchAnyOwningTags(CooldownTag.GetSingleTagContainer());
+		const float TImeRemaining = AbilitySystemC->GetActiveEffectsTimeRemaining(GameplayEffectQuery)[0];
+		CooldownStartDelegate.Broadcast(TImeRemaining);
+	}
+	
+}
